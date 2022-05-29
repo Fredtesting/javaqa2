c45658dbdeb0eb6ee3f0034f6183fa06b420282c
Voronchikhin Fedor <fredsergeevich@outlook.com>
2022-05-29 20:04:39 +0300

    Added files

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 100;
+        int mile = 20;
+        int bonus = amount / mile;
+        System.out.println(bonus + "бонусных миль" );
+    }
+}
